d29d967ff8ca5a7c1a376c6ca34482766eab837c
Modailun <modailun@gmail.com>
2025-08-11 21:54:04 +0200

    Added Birb

---
+++ Scripts/bird.gd
@@ -0,0 +1,6 @@
+extends RigidBody2D
+
+func _physics_process(delta: float) -> void:
+	if Input.is_action_just_pressed("Jump") :
+		print("jump")
+		apply_impulse(Vector2(0,-400), Vector2(0,0))
--- Scripts/kill_zone.gd
+++
@@ -1,5 +0,0 @@
-extends Area2D
-
-func _on_body_entered(body: Node2D) -> void:
-	print("You died.")
-	body.get_node("CollisionShape2D").queue_free()

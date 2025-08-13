bf3c2e7e68d5f5c6616eb68612f48aa1dbee13b4
Modailun <modailun@gmail.com>
2025-08-13 21:18:07 +0200

    Worked on the implamentation of the animations in GDScript

---
+++ Scripts/game_manager.gd
@@ -0,0 +1,9 @@
+extends Node
+
+@onready var animation_player: AnimationPlayer = $"../EveryPipes/Pipes/AnimationPlayer"
+
+func _ready() -> void:
+	pass
+
+func _process(delta: float) -> void:
+	pass
--- Scripts/pipes.gd
+++ Scripts/pipes.gd
@@ -2,6 +2,16 @@ extends AnimatableBody2D
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
+func _ready() -> void:
+	print("pipes ready")
+	var animation = animation_player.get_animation("Loop")
+	var track_index = animation.add_track(Animation.TYPE_VALUE)
+	animation.track_set_path(track_index,".:position:x")
+	animation.track_insert_key(track_index, 0.0, 0)
+	animation.track_insert_key(track_index, 10.0, 1200)
+	animation.length = 10.0
+	animation_player.play("Loop")
+
 
 func _on_point_body_exited(body: Node2D) -> void:
 	if body.is_class("RigidBody2D"):

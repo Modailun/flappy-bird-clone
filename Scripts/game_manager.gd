3a5a89668aeb7dd7e6fee92d26d12267f79b422b
Modailun <modailun@gmail.com>
2025-08-14 12:44:06 +0200

    Trying to fix pipes animation

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -3,7 +3,20 @@ extends Node
 @onready var animation_player: AnimationPlayer = $"../EveryPipes/Pipes/AnimationPlayer"
 
 func _ready() -> void:
-	pass
+	print("game manager ready")
+	#var animation = animation_player.get_animation("Loop")
+	#var track_index = animation.add_track(Animation.TYPE_VALUE)
+	#animation.track_set_path(track_index,".:position:x")
+	#animation.track_insert_key(track_index, 0.0, 1200)
+	#animation.track_insert_key(track_index, 10.0, -50)
+	#animation.length = 10.0
+	#animation.loop_mode = Animation.LOOP_LINEAR
+	#animation_player.play("Loop")
+	for i in range(5):  # Crée 5 objets
+		var objet = preload("res://scenes/pipes.tscn").instantiate()
+		objet.global_position.y = randf_range(160, 480)
+		objet.global_position.x = 1200
+		add_child(objet)
 
 func _process(delta: float) -> void:
 	pass
--- Scripts/pipes.gd
+++ Scripts/pipes.gd
@@ -4,14 +4,15 @@ extends AnimatableBody2D
 
 func _ready() -> void:
 	print("pipes ready")
+	
 	var animation = animation_player.get_animation("Loop")
 	var track_index = animation.add_track(Animation.TYPE_VALUE)
-	animation.track_set_path(track_index,".:position:x")
-	animation.track_insert_key(track_index, 0.0, 0)
-	animation.track_insert_key(track_index, 10.0, 1200)
+	animation.track_set_path(track_index,".:global_position:x")
+	animation.track_insert_key(track_index, 0.0, 1200)
+	animation.track_insert_key(track_index, 10.0, -50)
 	animation.length = 10.0
+	animation.loop_mode = Animation.LOOP_LINEAR
 	animation_player.play("Loop")
-
 
 func _on_point_body_exited(body: Node2D) -> void:
 	if body.is_class("RigidBody2D"):
